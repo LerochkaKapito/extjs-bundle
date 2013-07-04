b452da02c7f8922d46b7e4c7a99f63e4b3196844
James Moey <james.moey@tradingpursuits.com>
2013-07-04 13:46:36 +1000

    Added additional test case.

--- Tpg/ExtjsBundle/Tests/spec/restproxystore.spec.js
+++ Tpg/ExtjsBundle/Tests/spec/restproxystore.spec.js
@@ -7,7 +7,12 @@ describe('Rest Proxy in Model', function() {
         runs(function() {
             owner.save({
                 callback: function(record, operation, success) {
-                    runned = true;
+                    Test.TestBundle.Entity.CarOwner.load(record.getId(), {
+                        callback: function(record) {
+                            owner = record;
+                            runned = true;
+                        }
+                    });
                 }
             });
         });
@@ -129,6 +134,126 @@ describe('Rest Proxy in Model', function() {
         runs(function() {
             expect(car.getCarOwner()).not.toBeNull();
             expect(car.getCarOwner().getId()).toBeGreaterThan(0);
+        });
+    });
+    it('should update entity', function() {
+        var car = Ext.create('Test.TestBundle.Entity.Car', {
+            name: 'Ford',
+            plateNumber: 'AA1234',
+            password: 'xx'
+        });
+        var originalId;
+        var runned = false;
+        runs(function() {
+            car.save({
+                callback: function(record) {
+                    originalId = record.getId();
+                    record.set('name', 'BMW');
+                    record.save({
+                        callback: function(record) {
+                            car = record;
+                            runned = true;
+                        }
+                    })
+                }
+            })
+        });
+        waitsFor(function() {
+            return runned;
+        });
+        runs(function() {
+            expect(originalId).toBe(car.getId());
+            expect(car.get('name')).toBe('BMW');
+        });
+    })
+});
+describe('Rest Proxy in Store', function() {
+    var store = Ext.create('Ext.data.Store', {
+        model: 'Test.TestBundle.Entity.Car',
+        autoLoad: false,
+        proxy: {
+            type: 'rest',
+            url: '/mycars',
+            format: 'json'
+        }
+    });
+    it('should load all the entity', function() {
+        var runned = false;
+        runs(function() {
+            var car = Ext.create('Test.TestBundle.Entity.Car', {
+                name: 'Ford',
+                plateNumber: 'AA1234',
+                password: 'xx'
+            });
+            car.save({
+                callback: function() {
+                    store.load({
+                        callback: function() {
+                            runned = true;
+                        }
+                    });
+                }
+            });
+        });
+        waitsFor(function() {
+            return runned;
+        });
+        runs(function() {
+            expect(store.count()).toBeGreaterThan(0);
+        })
+    });
+    it('should remove all entity', function () {
+        var runned = false;
+        runs(function () {
+            store.load({
+                callback: function() {
+                    store.removeAll();
+                    store.sync({
+                        callback: function() {
+                            store.load({
+                                callback: function() {
+                                    runned = true;
+                                }
+                            })
+                        }
+                    })
+                }
+            });
+        });
+        waitsFor(function () {
+            return runned;
+        });
+        runs(function () {
+            expect(store.count()).toBe(0);
+        });
+    });
+    it('should add new entity', function () {
+        var runned = false;
+        runs(function () {
+            store.add([
+                {
+                    name: 'Ford',
+                    plateNumber: 'AA1234',
+                    password: 'xx'
+                },
+                {
+                    name: 'BMW',
+                    plateNumber: 'ZZ54',
+                    password: 'xx'
+                }
+            ]);
+            store.sync({
+                callback: function() {
+                    runned = true;
+                }
+            });
+        });
+        waitsFor(function () {
+            return runned;
+        });
+        runs(function () {
+            expect(store.count()).toBe(2);
+            expect(store.getNewRecords().length).toBe(0);
         });
     });
 });
