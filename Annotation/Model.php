744441ed1530c5490e6f7dd69512d8d2f49992bf
Nikita Sapogov <amstaffix@gmail.com>
2016-07-08 23:10:42 +0300

    added documentation to Model annotation

--- Annotation/Model.php
+++ Annotation/Model.php
@@ -1,14 +1,18 @@
 <?php
+
 namespace Tpg\ExtjsBundle\Annotation;
 
-use Doctrine\ORM\Mapping\Annotation;
-
 /**
+ * Mark class as extjs model
+ *
  * @Annotation
  * @Target("CLASS")
  */
-final class Model implements Annotation {
+final class Model {
+    /** @var string Extjs class name of extjs model */
     public $name;
+    /** @var string Extjs class to extend from */
     public $extend = "Ext.data.Model";
+    /** @var bool Generate Extjs class as base for further inheritance */
     public $generateAsBase = false;
 }
